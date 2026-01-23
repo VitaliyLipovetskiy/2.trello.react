355daf934599d6e8a8697a3888f4342ff610f872
Vitaliy Lipovetskiy <vitaliy1972li@gmail.com>
2026-01-23 22:47:56 +0200

    update css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React, { StrictMode } from 'react';
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import { Board, Home } from './pages/pages';
 import './App.css';
 
 function App() {
   return (
     <StrictMode>
-      <Router basename="/2.trello.react">
+      <Router>
         <Routes>
-          <Route index element={<Home />} />
-          <Route path={'board/:id'} element={<Board />} />
+          <Route path={'/'} element={<Home />} />
+          <Route path={'/board/:id'} element={<Board />} />
           <Route path={'*'} element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
